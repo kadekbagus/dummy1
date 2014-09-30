36ebd074d2b2f6b03d87a6860e425ae0af995169
Rio Astamal <me@rioastamal.net>
2014-09-30 10:12:09 +0700

    Add recursive route iterator and dummy route for example

--- app/routes.php
+++ app/routes.php
@@ -2,6 +2,28 @@
 
 /*
 |--------------------------------------------------------------------------
+| Orbit API Roites
+|--------------------------------------------------------------------------
+|
+| Search all php files inside the 'routes' the directory.
+|
+*/
+$directory = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'routes';
+$it = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directory));
+$it->rewind();
+while ($it->valid()) {
+    if (! $it->isDot()) {
+        // Only for php files
+        if ($it->getExtension() === 'php') {
+            $fullpath = $it->key();
+            require $fullpath;
+        }
+    }
+    $it->next();
+}
+
+/*
+|--------------------------------------------------------------------------
 | Application Routes
 |--------------------------------------------------------------------------
 |
@@ -13,5 +35,5 @@
 
 Route::get('/', function()
 {
-	return View::make('hello');
+    return View::make('hello');
 });
