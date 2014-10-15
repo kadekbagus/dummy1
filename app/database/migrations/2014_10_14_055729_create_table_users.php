e4c989ec3e8603ee87b047989ce6975fa5a20386
anz507 <anz507@gmail.com>
2014-10-15 15:09:41 +0800

    authentication migration with indexes

--- app/database/migrations/2014_10_14_055729_create_table_users.php
+++ app/database/migrations/2014_10_14_055729_create_table_users.php
@@ -3,42 +3,53 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableUsers extends Migration {
+class CreateTableUsers extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('users', function(Blueprint $table)
-		{
-			$table->bigInteger('user_id')->unsigned();
-			$table->string('username',50);
-			$table->string('user_password',100);
-			$table->string('user_email',255);
-			$table->string('user_firstname',50);
-			$table->string('user_lastname',75);
-			$table->string('user_status',20);
-			$table->dateTime('user_last_login');
-			$table->string('user_ip',45);
-			$table->integer('user_role_id')->unsigned();
-			$table->string('status',15);
-			$table->bigInteger('modified_by')->unsigned();
-			$table->timestamps();
-			$table->primary(array('user_id'));
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function(Blueprint $table)
+        {
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('username', 50);
+            $table->string('user_password', 100);
+            $table->string('user_email', 255);
+            $table->string('user_firstname', 50);
+            $table->string('user_lastname', 75);
+            $table->string('user_status', 20);
+            $table->dateTime('user_last_login');
+            $table->string('user_ip', 45);
+            $table->integer('user_role_id')->unsigned();
+            $table->string('status', 15);
+            $table->bigInteger('modified_by')->unsigned();
+            $table->timestamps();
+            $table->primary(array('user_id'));
+            $table->index(array('username'), 'username_idx');
+            $table->index(array('username', 'user_password'), 'username_pwd_idx');
+            $table->index(array('user_email'), 'email_idx');
+            $table->index(array('username', 'user_password', 'status'), 'username_pwd_status_idx');
+            $table->index(array('user_ip'), 'user_ip_idx');
+            $table->index(array('user_role_id'), 'user_role_id_idx');
+            $table->index(array('status'), 'status_idx');
+            $table->index(array('modified_by'), 'modified_by_idx');
+            $table->index(array('created_at'), 'created_at_idx');
+            $table->index(array('updated_at'), 'updated_at_idx');
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('users');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
 
 }
--- app/database/migrations/2014_10_14_083536_create_table_apikeys.php
+++ app/database/migrations/2014_10_14_083536_create_table_apikeys.php
@@ -3,35 +3,41 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableApikeys extends Migration {
+class CreateTableApikeys extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('apikeys', function(Blueprint $table)
-		{
-			$table->bigInteger('apikey_id')->unsigned();
-			$table->string('api_key',100);
-			$table->string('api_secret_key',255);
-			$table->bigInteger('user_id')->unsigned();
-			$table->string('status',15);
-			$table->timestamps();
-			$table->primary(array('apikey_id'));
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apikeys', function(Blueprint $table)
+        {
+            $table->bigInteger('apikey_id')->unsigned();
+            $table->string('api_key', 100);
+            $table->string('api_secret_key', 255);
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('status', 15);
+            $table->timestamps();
+            $table->primary(array('apikey_id'));
+            $table->index(array('api_key'), 'api_key_idx');
+            $table->index(array('user_id'), 'user_id_idx');
+            $table->index(array('api_key', 'user_id'), 'api_key_user_idx');
+            $table->index(array('api_key', 'status'), 'api_key_status_idx');
+            $table->index(array('api_key', 'user_id', 'status'), 'api_key_user_status_idx');
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('apikeys');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('apikeys');
+    }
 
 }
