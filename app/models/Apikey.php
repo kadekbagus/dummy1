c98c8e99eed39aa6398d4b11ffacf1c8a1525e3e
Rio Astamal <me@rioastamal.net>
2014-10-21 09:50:36 +0700

    Model Apikey: Add new method genApiKey() and genApiSecretKey()

--- app/models/Apikey.php
+++ app/models/Apikey.php
@@ -16,4 +16,37 @@ class Apikey extends Eloquent
     {
         return $this->belongsTo('User', 'user_id', 'user_id');
     }
+
+    /**
+     * Generate 40 random chars used for Api key.
+     *
+     * @author Rio Astamal <me@rioastamal.net>
+     * @param $user User - Instance of the User object
+     * @return string
+     */
+    public static function genApiKey($user)
+    {
+        $chars  = strrev($user->email) . $user->id . $user->created_at;
+        $chars .= microtime(TRUE);
+        $chars .= uniqid();
+
+        return sha1($chars);
+    }
+
+    /**
+     * Generate 64 random chars used for Api secret key.
+     *
+     * @author Rio Astamal <me@rioastamal.net>
+     * @param $user User - Instance of the User object
+     * @return string
+     */
+    public static function genSecretKey($user)
+    {
+        $chars = strrev($user->username) . $user->user_lastname . $user->user_firstname;
+        $chars .= strrev($user->email) . $user->id . $user->created_at;
+        $chars .= microtime(TRUE) . $user->user_password;
+        $chars .= uniqid() . mt_rand(0, 0xffffff);
+
+        return hash('sha256', $chars);
+    }
 }
