4c33d779f7754adfda8f7c730577b8d69f0589b7
Rio Astamal <me@rioastamal.net>
2014-10-22 10:38:33 +0700

    OrbitShop API v1: New library ControllerAPI to be used as base controller

---
+++ vendor/dominopos/orbit-shop-api/OrbitShop/API/v1/ControllerAPI.php
@@ -0,0 +1,153 @@
+<?php namespace OrbitShop\API\v1;
+/**
+ * Base API Controller.
+ *
+ * @author Rio Astamal <me@rioastamal.net>
+ */
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Response;
+
+abstract class ControllerAPI extends Controller
+{
+    /**
+     * The return value (response) of Controller API.
+     *
+     * @var ResponseAPI
+     */
+    public $response = NULL;
+
+    /**
+     * Direct access to the PHP PDO object which currently hold the connection.
+     *
+     * @var PDO
+     */
+    public $pdo = NULL;
+
+    /**
+     * The HTTP response type.
+     *
+     * @var string
+     */
+    public $contentType = 'application/json';
+
+    /**
+     * Store the Authentication.
+     *
+     * @var OrbitShopAPI
+     */
+    public $api;
+
+    /**
+     * Flag whether to use authentication or not.
+     *
+     * @var requireAuth
+     */
+    public $requireAuth = TRUE;
+
+    /**
+     * Maximum number of record that should be returned from query.
+     *
+     * @var int
+     */
+    public $maxRecord = 100;
+
+    /**
+     * Default number of record that should be returned if no limit spesified.
+     *
+     * @var int
+     */
+    public $defaultNumberOfRecord = 20;
+
+    /**
+     * How long request should considered invalid in seconds.
+     *
+     * @var int
+     */
+    public $expiresTime = 60;
+
+    /**
+     * Contructor
+     *
+     * @param string $content_type - HTTP content type that would be sent to client
+     */
+    public function __construct($contentType = 'application/json') {
+        // default content type set to JSON
+        $this->contentType = $contentType;
+
+        // Set the default response
+        $this->response = new ResponseProvider();
+    }
+
+    /**
+     * Method to authenticate the API consumer.
+     *
+     * @author Rio Astamal <me@rioastamal.net>
+     *
+     * @param string $input_secret_name - Default name for getting secret key input.
+     * @return void
+     * @thrown Exception
+     */
+    protected function checkAuth()
+    {
+        // Get the api key from query string
+        $clientkey = (isset($_GET['apikey']) ? $_GET['apikey'] : '');
+
+        // Instantiate the OrbitShopAPI
+        $this->api = new OrbitShopAPI($clientkey);
+
+        // Set the request expires time
+        $this->api->expiresTimeFrame = $this->expiresTime;
+
+        // Run the signature check routine
+        $this->api->checkSignature();
+    }
+
+    /**
+     * Return the output of the API to the caller.
+     *
+     * @author Rio Astamal <me@rioastamal.net>
+     * @param int $httpCode - The HTTP status code response.
+     * @return OrbitShop\API\v1\ResponseProvider | string
+     */
+    protected function render($httpCode=200)
+    {
+        $output = '';
+
+        switch ($this->contentType) {
+            case 'raw':
+                return $this->response;
+                break;
+
+            case 'application/json':
+            default:
+                $json = new \stdClass();
+                $json->code = $this->response->code;
+                $json->status = $this->response->status;
+                $json->message = $this->response->message;
+                $json->data = $this->response->data;
+
+                $output = json_encode($json);
+        }
+
+        return Response::make($output, $httpCode, array('Content-Type' => $this->contentType));
+    }
+
+    /**
+     * Magic method which alled when calling undefined method handler on
+     * controller.
+     *
+     * @author Rio Astamal <me@rioastamal.net>
+     * @param string $method - The method name
+     * @param array $args - The arguments
+     * @return OrbitShop\API\v1\ResponseProvider | string
+     */
+    public function __call($method, $args)
+    {
+        $this->response->code = 5;
+        $this->response->status = 'error';
+        $this->response->message = 'Request URL not found';
+        $this->response->data = NULL;
+
+        return $this->render();
+    }
+}
