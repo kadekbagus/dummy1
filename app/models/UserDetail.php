141bb559a07af74d77c414a04309e16f2e243a47
Ahmad Anshori <ahmad@dominopos.com>
2014-10-23 19:08:10 +0800

    Add new relationship: user, modifier, scopeActive to UserDetailModel

--- app/models/UserDetail.php
+++ app/models/UserDetail.php
@@ -8,11 +8,16 @@ class UserDetail extends Eloquent
 
     public function user()
     {
-        return $this->belongsTo('User', 'user_id', 'user_id');
+        return $this->belongsTo('User', 'user_id', 'user_id')->where('users.status', '=', 'active');
     }
 
     public function modifier()
     {
         return $this->belongsTo('User', 'modified_by', 'user_id');
+    }
+
+    public function scopeActive($query)
+    {
+        return $query->join('users', 'user_details.user_id', '=', 'users.user_id')->where('users.status', '=', 'active');
     }
 }
