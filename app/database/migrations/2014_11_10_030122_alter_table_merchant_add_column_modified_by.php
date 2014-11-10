a3dfd03a8de37006b2049e9e08d31c489debcfed
Ahmad Anshori <ahmad@dominopos.com>
2014-11-10 11:12:19 +0800

    Migration Merchants Table : fix coding standard

--- app/database/migrations/2014_11_10_030122_alter_table_merchant_add_column_modified_by.php
+++ app/database/migrations/2014_11_10_030122_alter_table_merchant_add_column_modified_by.php
@@ -3,34 +3,32 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterTableMerchantAddColumnModifiedBy extends Migration {
+class AlterTableMerchantAddColumnModifiedBy extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('merchants', function (Blueprint $table) {
+            $table->bigInteger('modified_by')->unsigned()->default(0)->after('parent_id');
+            $table->index(array('modified_by'), 'modified_by_idx');
+        });
+    }
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::table('merchants', function(Blueprint $table)
-		{
-			$table->bigInteger('modified_by')->unsigned()->default(0)->after('parent_id');
-			$table->index(array('modified_by'), 'modified_by_idx');
-		});
-	}
-
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::table('merchants', function(Blueprint $table)
-		{
-			$table->dropIndex('modified_by_idx');
-			$table->dropColumn('modified_by');
-		});
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('merchants', function (Blueprint $table) {
+            $table->dropIndex('modified_by_idx');
+            $table->dropColumn('modified_by');
+        });
+    }
 
 }
