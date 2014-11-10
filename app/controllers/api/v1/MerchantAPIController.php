14ddae9724317b2dd1e7dbbc16325e5c62d60c0e
kadek <kadek@dominopos.com>
2014-11-10 14:30:04 +0800

    add postUpdateMerchant() on MerchantAPIController

--- app/controllers/api/v1/MerchantAPIController.php
+++ app/controllers/api/v1/MerchantAPIController.php
@@ -747,6 +747,214 @@ class MerchantAPIController extends ControllerAPI
         return $this->render($httpCode);
     }
 
+    /**
+     * POST - Update merchant
+     *
+     * @author <Kadek> <kadek@dominopos.com>
+     *
+     * List of API Parameters
+     * ----------------------
+     * @param integer    `merchant_id`           (required) - ID of the merchant
+     * @param integer    `user_id`               (required) - User id for the merchant
+     * @param string     `email`                 (required) - Email address of the merchant
+     * @param string     `name`                  (optional) - Name of the merchant
+     * @param string     `description`           (optional) - Merchant description
+     * @param string     `address_line1`         (optional) - Address 1
+     * @param string     `address_line2`         (optional) - Address 2
+     * @param string     `address_line3`         (optional) - Address 3
+     * @param integer    `city_id`               (optional) - City id
+     * @param string     `city`                  (optional) - Name of the city
+     * @param integer    `country_id`            (optional) - Country id
+     * @param string     `country`               (optional) - Name of the country
+     * @param string     `phone`                 (optional) - Phone of the merchant
+     * @param string     `fax`                   (optional) - Fax of the merchant
+     * @param string     `start_date_activity`   (optional) - Start date activity of the merchant
+     * @param string     `status`                (optional) - Status of the merchant
+     * @param string     `logo`                  (optional) - Logo of the merchant
+     * @param string     `currency`              (optional) - Currency used by the merchant
+     * @param string     `currency_symbol`       (optional) - Currency symbol
+     * @param string     `tax_code1`             (optional) - Tax code 1
+     * @param string     `tax_code2`             (optional) - Tax code 2
+     * @param string     `tax_code3`             (optional) - Tax code 3
+     * @param string     `slogan`                (optional) - Slogan for the merchant
+     * @param string     `vat_included`          (optional) - Vat included
+     * @param string     `object_type`           (optional) - Object type
+     * @param string     `parent_id`             (optional) - The merchant id
+     * @return Illuminate\Support\Facades\Response
+     */
+    public function postUpdateMerchant()
+    {
+        try {
+            $httpCode=200;
+
+            Event::fire('orbit.user.postupdatemerchant.before.auth', array($this));
+
+            // Require authentication
+            $this->checkAuth();
+
+            Event::fire('orbit.user.postupdatemerchant.after.auth', array($this));
+
+            // Try to check access control list, does this user allowed to
+            // perform this action
+            $user = $this->api->user;
+            Event::fire('orbit.user.postupdatemerchant.before.authz', array($this, $user));
+
+            if (! ACL::create($user)->isAllowed('update_merchant')) {
+                Event::fire('orbit.user.postupdatemerchant.authz.notallowed', array($this, $user));
+                $updateMerchantLang = Lang::get('validation.orbit.actionlist.update_merchant');
+                $message = Lang::get('validation.orbit.access.forbidden', array('action' => $updateMerchantLang));
+                ACL::throwAccessForbidden($message);
+            }
+            Event::fire('orbit.user.postupdatemerchant.after.authz', array($this, $user));
+
+            $this->registerCustomValidation();
+
+            $merchant_id = OrbitInput::post('merchant_id');
+            $user_id = OrbitInput::post('user_id');
+            $email = OrbitInput::post('email');
+            $name = OrbitInput::post('name');
+            $description = OrbitInput::post('description');
+            $address_line1 = OrbitInput::post('address_line1');
+            $address_line2 = OrbitInput::post('address_line2');
+            $address_line3 = OrbitInput::post('address_line3');
+            $city_id = OrbitInput::post('city_id');
+            $city = OrbitInput::post('city');
+            $country_id = OrbitInput::post('country_id');
+            $country = OrbitInput::post('country');
+            $phone = OrbitInput::post('phone');
+            $fax = OrbitInput::post('fax');
+            $start_date_activity = OrbitInput::post('start_date_activity');
+            $status = OrbitInput::post('status');
+            $logo = OrbitInput::post('logo');
+            $currency = OrbitInput::post('currency');
+            $currency_symbol = OrbitInput::post('currency_symbol');
+            $tax_code1 = OrbitInput::post('tax_code1');
+            $tax_code2 = OrbitInput::post('tax_code2');
+            $tax_code3 = OrbitInput::post('tax_code3');
+            $slogan = OrbitInput::post('slogan');
+            $vat_included = OrbitInput::post('vat_included');
+            $object_type = OrbitInput::post('object_type');
+            $parent_id = OrbitInput::post('parent_id');
+
+            $validator = Validator::make(
+                array(
+                    'merchant_id'       => $merchant_id,
+                    'user_id'           => $user_id,
+                    'email'             => $email,
+                ),
+                array(
+                    'merchant_id'       => 'required|numeric',
+                    'user_id'           => 'required|numeric',
+                    'email'             => 'required|email|orbit.email.exists',
+                )
+            );
+
+            Event::fire('orbit.user.postupdatemerchant.before.validation', array($this, $validator));
+
+            // Run the validation
+            if ($validator->fails()) {
+                $errorMessage = $validator->messages()->first();
+                OrbitShopAPI::throwInvalidArgument($errorMessage);
+            }
+            Event::fire('orbit.user.postupdatemerchant.after.validation', array($this, $validator));
+
+            // Begin database transaction
+            $this->beginTransaction();
+
+            $updatedmerchant = Merchant::find($merchant_id);
+            $updatedmerchant->user_id = $user_id;
+            $updatedmerchant->email = $email;
+            $updatedmerchant->name = $name;
+            $updatedmerchant->description = $description;
+            $updatedmerchant->address_line1 = $address_line1;
+            $updatedmerchant->address_line2 = $address_line2;
+            $updatedmerchant->address_line3 = $address_line3;
+            $updatedmerchant->city_id = $city_id;
+            $updatedmerchant->city = $city;
+            $updatedmerchant->country_id = $country_id;
+            $updatedmerchant->country = $country;
+            $updatedmerchant->phone = $phone;
+            $updatedmerchant->fax = $fax;
+            $updatedmerchant->start_date_activity = $start_date_activity;
+            $updatedmerchant->status = $status;
+            $updatedmerchant->logo = $logo;
+            $updatedmerchant->currency = $currency;
+            $updatedmerchant->currency_symbol = $currency_symbol;
+            $updatedmerchant->tax_code1 = $tax_code1;
+            $updatedmerchant->tax_code2 = $tax_code2;
+            $updatedmerchant->tax_code3 = $tax_code3;
+            $updatedmerchant->slogan = $slogan;
+            $updatedmerchant->vat_included = $vat_included;
+            $updatedmerchant->object_type = $object_type;
+            $updatedmerchant->parent_id = $parent_id;
+            $updatedmerchant->modified_by = $this->api->user->user_id;
+
+            Event::fire('orbit.user.postupdatemerchant.before.save', array($this, $updatedmerchant));
+
+            $updatedmerchant->save();
+
+            Event::fire('orbit.user.postupdatemerchant.after.save', array($this, $updatedmerchant));
+            $this->response->data = $updatedmerchant->toArray();
+
+            // Commit the changes
+            $this->commit();
+
+            Event::fire('orbit.user.postupdatemerchant.after.commit', array($this, $updatedmerchant));
+        } catch (ACLForbiddenException $e) {
+            Event::fire('orbit.user.postupdatemerchant.access.forbidden', array($this, $e));
+
+            $this->response->code = $e->getCode();
+            $this->response->status = 'error';
+            $this->response->message = $e->getMessage();
+            $this->response->data = null;
+            $httpCode = 403;
+
+            // Rollback the changes
+            $this->rollBack();
+        } catch (InvalidArgsException $e) {
+            Event::fire('orbit.user.postupdatemerchant.invalid.arguments', array($this, $e));
+
+            $this->response->code = $e->getCode();
+            $this->response->status = 'error';
+            $this->response->message = $e->getMessage();
+            $this->response->data = null;
+            $httpCode = 403;
+
+            // Rollback the changes
+            $this->rollBack();
+        } catch (QueryException $e) {
+            Event::fire('orbit.user.postupdatemerchant.query.error', array($this, $e));
+
+            $this->response->code = $e->getCode();
+            $this->response->status = 'error';
+
+            // Only shows full query error when we are in debug mode
+            if (Config::get('app.debug')) {
+                $this->response->message = $e->getMessage();
+            } else {
+                $this->response->message = Lang::get('validation.orbit.queryerror');
+            }
+            $this->response->data = null;
+            $httpCode = 500;
+
+            // Rollback the changes
+            $this->rollBack();
+        } catch (Exception $e) {
+            Event::fire('orbit.user.postupdatemerchant.general.exception', array($this, $e));
+
+            $this->response->code = $e->getCode();
+            $this->response->status = 'error';
+            $this->response->message = $e->getMessage();
+            $this->response->data = null;
+
+            // Rollback the changes
+            $this->rollBack();
+        }
+
+        return $this->render($httpCode);
+
+    }
+
     protected function registerCustomValidation()
     {
         // Check the existance of merchant id
