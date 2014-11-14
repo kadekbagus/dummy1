1808123b567188a7f9820699d10651285096a26a
kadek <kadek@dominopos.com>
2014-11-14 10:58:41 +0800

    migration add category_family_level column and change table engine

---
+++ app/database/migrations/2014_11_14_023311_alter_product_category_add_category_family_level_column.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterProductCategoryAddCategoryFamilyLevelColumn extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('product_category', function(Blueprint $table)
+		{
+			$table->integer('category_family_level')->unsigned()->nullable()->after('product_id');
+			$table->index(array('category_family_level'), 'category_family_level_idx');
+			$table->index(array('category_id', 'category_family_level'), 'categoryid_family_level_idx');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::table('user_details', function (Blueprint $table) {
+            $table->dropColumn('category_family_level');
+            $table->dropIndex('category_family_level_idx');
+            $table->dropIndex('categoryid_family_level_idx');
+        });
+	}
+
+}
