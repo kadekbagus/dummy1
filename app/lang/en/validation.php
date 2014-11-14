0706ff88a8f83c0843a319614a12914225e6f42a
Tian <tian@dominopos.com>
2014-11-14 17:01:31 +0800

    Create unit test for MerchantAPIController::postUpdateMerchant()

--- app/lang/en/validation.php
+++ app/lang/en/validation.php
@@ -102,6 +102,7 @@ return array(
             'merchant'      => 'The Merchant ID you specify is not found.',
             'user_status'   => 'The user status you specify is not found.',
             'user_sortby'   => 'The sort by argument you specify is not valid, the valid value are: username, email, firstname, lastname, and registered_date.',
+            'merchant_status'   => 'The merchant status you specify is not found.',
         ),
         'queryerror' => 'Database query error, turn on debug mode to see the full query.',
         'actionlist' => array(
