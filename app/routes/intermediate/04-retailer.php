2a45a6f33a88f7c5be5f05ab7f6a7ca6091d1734
Ahmad Anshori <ahmad@dominopos.com>
2014-11-24 16:54:52 +0800

    Retailer Intermediate Routes

---
+++ app/routes/intermediate/04-retailer.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Routes file for Intermediate Retailer API
+ */
+
+/**
+ * Create new retailer
+ */
+Route::post('/app/v1/retailer/new', 'IntermediateAuthController@Retailer_postNewRetailer');
+
+/**
+ * Delete retailer
+ */
+Route::post('/app/v1/retailer/delete', 'IntermediateAuthController@Retailer_postDeleteRetailer');
+
+/**
+ * Update retailer
+ */
+Route::post('/app/v1/retailer/update', 'IntermediateAuthController@Retailer_postUpdateRetailer');
+
+/**
+ * List and/or Search retailer
+ */
+Route::get('/app/v1/retailer/search', 'IntermediateAuthController@Retailer_getSearchRetailer');
