d1cbb297a5d843d24a5e773beb9311a31288837e
Rio Astamal <me@rioastamal.net>
2014-11-24 15:35:07 +0700

    Disable the Authentication

--- app/controllers/intermediate/v1/IntermediateAuthController.php
+++ app/controllers/intermediate/v1/IntermediateAuthController.php
@@ -23,10 +23,17 @@ class IntermediateAuthController extends IntermediateBaseController
         {
             try
             {
+                $user = User::find(1);
+
+                // Hard code the login as off 24-11-2014, since the frontend
+                // having difficulty with AngularJS
+                Auth::login( $user );
+                /*
                 if (! Auth::check()) {
                     $message = Lang::get('validation.orbit.access.needtologin');
                     ACL::throwAccessForbidden($message);
                 }
+                */
             } catch (ACLForbiddenException $e) {
                 $response = new ResponseProvider();
                 $response->code = $e->getCode();
