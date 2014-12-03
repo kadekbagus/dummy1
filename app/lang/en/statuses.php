e7f18bbbb1ade315ccfe84bbee419c42ab2cbde3
Tian <tian@dominopos.com>
2014-12-03 17:36:08 +0800

    Add 'statuses.orbit.nodata.tax'

--- app/lang/en/statuses.php
+++ app/lang/en/statuses.php
@@ -25,6 +25,7 @@ return array(
             'merchant'  => 'There is no merchant found that matched your criteria.',
             'retailer'  => 'There is no retailer found that matched your criteria.',
             'product'   => 'There is no product found that matched your criteria.',
+            'tax'       => 'There is no tax found that matched your criteria.',
         ),
     )
 );
