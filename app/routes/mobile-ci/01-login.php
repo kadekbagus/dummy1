942602fa1e04f712e2de9e49cf3021535b2e364a
Ahmad Anshori <ahmad@dominopos.com>
2014-12-15 19:54:59 +0800

    LoginAPIController : create new method for mobile CI

--- app/routes/mobile-ci/01-login.php
+++ app/routes/mobile-ci/01-login.php
@@ -1,6 +1,11 @@
 <?php
 
-Route::get('/head', function() 
+Route::get('/customer', function() 
 {
     return View::make('mobile-ci.signin');
+});
+
+Route::get('/api/v1/customer/login', function() 
+{
+    return LoginAPIController::create()->postLoginInShop();
 });
