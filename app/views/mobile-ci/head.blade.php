91a6027ac18bf796ae944bd3323ed640e8bb526c
Danish Kalesaran <danish@dominopos.com>
2014-12-15 17:29:55 +0800

    initializing templates

---
+++ app/views/mobile-ci/head.blade.php
@@ -0,0 +1,7 @@
+<meta charset="utf-8" />
+<meta name="description" content="Orbit app mobile customer interface" />
+<meta name="author" content="DominoPOS" />
+
+<!-- <title>Orbit</title> -->
+
+{{ HTML::style('mobile-ci/stylesheet/main.css') }}
---
+++ app/views/mobile-ci/layout-headless.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+	<head>
+		@include('mobile-ci.head')
+	</head>
+	<body>
+		<div class="container">
+      @yield('content')
+    </div>
+	</body>
+</html>
