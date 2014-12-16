343618c9b842435a49c2fa1ec9846dc7f05cf4b8
Ahmad Anshori <ahmad@dominopos.com>
2014-12-16 16:27:54 +0800

    MobileCI : Signin/SignUp

--- app/routes/mobile-ci/01-login.php
+++ app/routes/mobile-ci/01-login.php
@@ -1,20 +1,20 @@
 <?php
 
 // -------------------- views ------------------------------
-Route::get('/customer', function() 
+Route::get('/customer', array('as' => 'signin', function() 
 {
     return View::make('mobile-ci.signin');
-});
+}));
 Route::get('/customer/signup', function() 
 {
-    return View::make('mobile-ci.signup');
+    return View::make('mobile-ci.signup', array('email' => ''));
 });
 
 Route::post('/customer/signup', array('uses'=>'MobileCI\\MobileCIAPIController@postSignUpView'));
 
 Route::group(array('before'=>'authCustomer'), function()
 {
-    Route::get('/', function() 
+    Route::get('/customer/home', function() 
     {
         return View::make('mobile-ci.home');
     });
@@ -22,9 +22,28 @@ Route::group(array('before'=>'authCustomer'), function()
 // -------------------- views ------------------------------
 
 
+// Route::post('/api/v1/customer/check', function() 
+// {
+//     return MobileCI\MobileCIAPIController::create()->postCheckEmail();
+// });
+
 Route::post('/api/v1/customer/login', function() 
 {
     return MobileCI\MobileCIAPIController::create()->postLoginInShop();
 });
 
 Route::post('/app/v1/customer/login', 'IntermediateAuthController@MobileCI_postLoginInShop');
+
+Route::get('/api/v1/customer/logout', function() 
+{
+    return MobileCI\MobileCIAPIController::create()->getLogoutInShop();
+});
+
+Route::get('/app/v1/customer/logout', 'IntermediateAuthController@MobileCI_getLogoutInShop');
+
+Route::post('/api/v1/customer/signup', function() 
+{
+    return MobileCI\MobileCIAPIController::create()->postRegisterUserInShop();
+});
+
+Route::post('/app/v1/customer/signup', 'IntermediateAuthController@MobileCI_postRegisterUserInShop');
