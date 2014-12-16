ad3a52f8fb6b491990d56e3cbccf7e6516ee2a04
kadek <kadek@dominopos.com>
2014-12-16 11:36:30 +0800

    add controller and route for login and logout cashier

---
+++ app/routes/pos/01-login.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Routes file for POS quick & dirty
+ */
+
+Route::post('/api/v1/pos/login', function () {
+    return LoginAPIController::create()->postLoginCashier();
+});
+
+Route::post('/app/v1/pos/login', 'IntermediateAuthController@Login_postLoginCashier');
+
+Route::get('/pos', function () {
+    if (Auth::check()) {
+
+    } else {
+        return View::make('pos.login');
+    }
+});
+
+Route::get('/pos/home', function () {
+    if (Auth::check()) {
+        echo "anda login <br/>";
+        $user_id = Auth::user()->user_id;
+        $username = Auth::user()->username;
+        $email = Auth::user()->user_email;
+        echo "user id ".$user_id."<br/>";
+        echo "username ".$username."<br/>";
+        echo "email ".$email."<br/>";
+    } else {
+        echo "anda tidak login";
+    }
+});
+
+Route::get('/pos/logout', function () {
+    Auth::logout();
+});
