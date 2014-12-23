5892b7e2ddf2935d8087bfb36e9b26316b402ad4
Rio Astamal <me@rioastamal.net>
2014-12-23 10:41:55 +0700

    Views activation email: Change frontend URL to dummy

--- app/views/emails/registration/activation-html.blade.php
+++ app/views/emails/registration/activation-html.blade.php
@@ -2,7 +2,7 @@
 <p>We hope you had a great shopping experience at <b>&quot;{{ $shop_name }}&quot;</b>.
 To complete your registration and obtain great promotion and money saving deals, follow the link below:
 <br>
-<a href="{{ $token_url }}">{{ $token_url}}</a></p>
+<a href="{ #FRONTEND_URL }">{ #FRONTEND_URL }</a></p>
 
 <p>Happy shopping,
 <br><br>
--- app/views/emails/registration/activation-text.blade.php
+++ app/views/emails/registration/activation-text.blade.php
@@ -4,7 +4,7 @@ We hope you had a great shopping experience at "{{ $shop_name }}".
 To complete your registration and obtain great promotion and money saving deals,
 follow the link below:
 
-{{ $token_url }}
+{ #FRONTEND_URL }
 
 Happy shopping,
 
