42b441b207e5c03da2b42834140aa88339297900
Rio Astamal <me@rioastamal.net>
2014-12-23 11:02:20 +0700

    Views email activation: Going back to {{ $token_url }}

--- app/views/emails/registration/activation-text.blade.php
+++ app/views/emails/registration/activation-text.blade.php
@@ -4,7 +4,7 @@ We hope you had a great shopping experience at "{{ $shop_name }}".
 To complete your registration and obtain great promotion and money saving deals,
 follow the link below:
 
-{ #FRONTEND_URL }
+{{ $token_url }}
 
 Happy shopping,
 
