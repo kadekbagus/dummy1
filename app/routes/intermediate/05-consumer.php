fc22dff03231b4dd4282133fff463e7f4cea9d75
Rio Astamal <me@rioastamal.net>
2014-12-25 06:14:32 +0700

    Consumer routes: add an alias for consumer listing /app/v1/consumer/list

--- app/routes/intermediate/05-consumer.php
+++ app/routes/intermediate/05-consumer.php
@@ -8,3 +8,4 @@
  * List and/or Search consumer
  */
 Route::get('/app/v1/consumer/search', 'IntermediateAuthController@User_getConsumerListing');
+Route::get('/app/v1/consumer/list', 'IntermediateAuthController@User_getConsumerListing');
