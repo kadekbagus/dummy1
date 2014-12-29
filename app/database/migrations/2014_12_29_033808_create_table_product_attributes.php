f91c8a459b6d289a8337d1f36ad81d6c7b3a7dbd
Rio Astamal <me@rioastamal.net>
2014-12-29 11:08:45 +0700

    New migration: Create table product_attributes

---
+++ app/database/migrations/2014_12_29_033808_create_table_product_attributes.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableProductAttributes extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_attributes', function(Blueprint $table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('product_attribute_id');
+            $table->string('product_attribute_name', '50')->nullable();
+            $table->timestamps();
+
+            $table->index(array('product_attribute_name'), 'product_attribute_name_idx');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('product_attributes');
+    }
+}
--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit5b2b7ec2b1710e67d9f0b4d8315aa7cd::getLoader();
+return ComposerAutoloaderInit5b5723cb0447e95f0b1ab30b00bf0b44::getLoader();
