376f6ae8bc6407d6b2d5d2559b1647dfc07ca223
julisman <agung.julisman@yahoo.com>
2014-12-31 16:42:36 +0800

    fix bug get product

--- public/templatepos/js/app.js
+++ public/templatepos/js/app.js
@@ -103,9 +103,8 @@ var app = angular.module('app', ['ui.bootstrap','ngAnimate','LocalStorageModule'
         $scope.getproduct = function(){
             if(progressJs) progressJs("#loading").start().autoIncrease(4, 500);
             serviceAjax.getDataFromServer('/product/search?merchant_id[]=' + $scope.datauser['userdetail']['merchant_id'] + '&take=14').then(function(response){
-                console.log(response);
                 if(response.code == 0 ){
-                    for(var i =0; i <response.data.records.length; i++){
+                    if(response.data.records.length > 0)for(var i =0; i <response.data.records.length; i++){
                        response.data.records[i]['price'] = accounting.formatMoney(response.data.records[i]['price'], "", 0, ",", ".");
                     }
                     $scope.product = response.data.records;
