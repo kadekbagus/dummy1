97d06de5c984a5dbbe4cf21829a1122f644dc883
Tian <tian@dominopos.com>
2015-01-08 12:02:27 +0800

    Create route files for Coupon.

---
+++ app/routes/api/11-coupon.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Routes file for Coupon related API
+ */
+
+/**
+ * Create new coupon
+ */
+Route::post('/api/v1/coupon/new', function()
+{
+    return CouponAPIController::create()->postNewCoupon();
+});
+
+/**
+ * Delete coupon
+ */
+Route::post('/api/v1/coupon/delete', function()
+{
+    return CouponAPIController::create()->postDeleteCoupon();
+});
+
+/**
+ * Update coupon
+ */
+Route::post('/api/v1/coupon/update', function()
+{
+    return CouponAPIController::create()->postUpdateCoupon();
+});
+
+/**
+ * List/Search coupon
+ */
+Route::get('/api/v1/coupon/search', function()
+{
+    return CouponAPIController::create()->getSearchCoupon();
+});
