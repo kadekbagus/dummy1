a7d80140a26a2c2647669f18c33882f6e2d44e06
Rio Astamal <me@rioastamal.net>
2015-01-20 14:50:33 +0800

    New routes: /app/v1/widget/update

--- app/routes/api/12-widget.php
+++ app/routes/api/12-widget.php
@@ -11,3 +11,11 @@ Route::post('/api/v1/widget/new', function()
     return WidgetAPIController::create()->postNewWidget();
 });
 
+/**
+ * Update Widget
+ */
+Route::post('/api/v1/widget/update', function()
+{
+    return WidgetAPIController::create()->postUpdateWidget();
+});
+
--- app/routes/intermediate/12-widget.php
+++ app/routes/intermediate/12-widget.php
@@ -7,3 +7,8 @@
  * Create New Widget
  */
 Route::post('/app/v1/widget/new', 'IntermediateAuthController@Widget_postNewWidget');
+
+/**
+ * Update Widget
+ */
+Route::post('/app/v1/widget/update', 'IntermediateAuthController@Widget_postUpdateWidget');
