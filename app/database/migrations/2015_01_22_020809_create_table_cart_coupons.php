f92d6926f5f3a35acd4844a1fd9a5d6df5f4f0e8
Ahmad Anshori <ahmad@dominopos.com>
2015-01-22 10:19:51 +0800

    MobileCI : Fix typo in migration for cart_coupons table

--- app/database/migrations/2015_01_22_020809_create_table_cart_coupons.php
+++ app/database/migrations/2015_01_22_020809_create_table_cart_coupons.php
@@ -3,39 +3,40 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableCartCoupons extends Migration {
+class CreateTableCartCoupons extends Migration 
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('cart_coupons', function(Blueprint $table)
-		{
-			$table->engine = 'InnoDB';
-			$table->bigIncrements('cart_coupon_id');
-			$table->bigInteger('issued_coupon_id');
-			$table->string('object_type', 50);
-			$table->bigInteger('object_id');
-			$table->timestamps();
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_coupons', function(Blueprint $table)
+        {
+            $table->engine = 'InnoDB';
+            $table->bigIncrements('cart_coupon_id');
+            $table->bigInteger('issued_coupon_id');
+            $table->string('object_type', 50);
+            $table->bigInteger('object_id');
+            $table->timestamps();
 
-			$table->index(array('cart_coupon_id'), 'cart_couponid_idx');
-			$table->index(array('issued_coupon_id'), 'issued_couponid_idx');
-			$table->index(array('object_type'), 'object_type_idx');
-			$table->index(array('object_id'), 'objectid_idx');
-		});
-	}
+            $table->index(array('cart_coupon_id'), 'cart_couponid_idx');
+            $table->index(array('issued_coupon_id'), 'issued_couponid_idx');
+            $table->index(array('object_type'), 'object_type_idx');
+            $table->index(array('object_id'), 'objectid_idx');
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('cart_coupons');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cart_coupons');
+    }
 
 }
