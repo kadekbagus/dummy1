7a95e5dab536f195539edb08277a9b2045077165
Rio Astamal <me@rioastamal.net>
2015-01-28 17:43:41 +0800

    New routes: /app/v1/consumer-transaction-history/product-list

--- app/routes/api/18-transaction.php
+++ app/routes/api/18-transaction.php
@@ -18,3 +18,11 @@ Route::get('/api/v1/consumer-transaction-history/retailer-list', function()
 {
     return TransactionHistoryAPIController::create()->getMerchantList();
 });
+
+/**
+ * Get list of product for particular user which has transactions
+ */
+Route::get('/api/v1/consumer-transaction-history/product-list', function()
+{
+    return TransactionHistoryAPIController::create()->getProductList();
+});
