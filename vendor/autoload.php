16531d781dfc2a8853f24bdacc15bde59327d63e
Tian <tian@dominopos.com>
2015-02-11 18:21:59 +0800

    Create migration for table events to add column widget_object_type.

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit255eddf2ea87f700476e3a486e60e22e::getLoader();
+return ComposerAutoloaderInit3bae1664940ab84ef8d7e27f99b77f7d::getLoader();
