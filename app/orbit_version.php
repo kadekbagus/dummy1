4b5aaf7127a93bec2d6b91aeeb63df5323faeb80
Rio Astamal <me@rioastamal.net>
2015-03-06 12:16:47 +0800

    Introducing new file for storing application version app/orbit_version.php

---
+++ app/orbit_version.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * File which holds the version of Orbit Application.
+ *
+ * @author Rio Astamal <me@rioastamal.net>
+ */
+
+/**
+ * Main constant storing app version.
+ */
+define('ORBIT_APP_VERSION', '1.0');
+
+/**
+ * Main constant storing app version for internal use.
+ */
+define('ORBIT_APP_INTERNAL_VERSION', '1.0.0');
+
+/**
+ * Constant storing codename.
+ */
+define('ORBIT_APP_CODENAME', 'Sputnik');
+
+/**
+ * Constant storing the release date, ISO 8601.
+ */
+define('ORBIT_APP_RELEASE_DATE', '');
