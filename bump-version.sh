9de701fcb957aa6ef11abacd94da3ee7d791ea74
Rio Astamal <me@rioastamal.net>
2015-03-12 23:38:38 +0800

    bump-version: GIT_SSH using the absolute path of jenkins home

--- bump-version.sh
+++ bump-version.sh
@@ -19,9 +19,10 @@ echo "Bumping build date to ${BUILD_ID}..."
 sed -i "s/\(ORBIT_APP_BUILD_DATE\x27,\)\s\(\x27*\x27\));/\1 \x27$BUILD_ID\x27);/" ${PHP_VERSION_PATH}
 
 # Commit to the origin repository
+echo "JENKINS HOME_DIR=${JENKINS_HOME}"
 echo "Committing the changes for build number ${BUILD_NUMBER}..."
 git add ${PHP_VERSION_PATH} && git commit -m "Jenkins: Bump build number to ${BUILD_NUMBER}"
 
 echo "Pushing new build number ${BUILD_NUMBER} to github..."
-export GIT_SSH=~/git-orbit-ssh.sh
-ORBIT_SHOP_KEY=~/orbit-deploy-keys/orbit-shop-deploy.key git push origin $(cat .git/HEAD):development
+export GIT_SSH=${JENKINS_HOME}/git-orbit-ssh.sh
+ORBIT_SHOP_KEY=${JENKINS_HOME}/orbit-deploy-keys/orbit-shop-deploy.key git push origin $(cat .git/HEAD):development
