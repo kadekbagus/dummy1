597cede7a00fbd84b61c10745407e8979c31bb78
Rio Astamal <me@rioastamal.net>
2015-03-12 22:53:21 +0800

    bump-version: Replace the sed regex

--- bump-version.sh
+++ bump-version.sh
@@ -4,12 +4,6 @@
 # @desc Increment the build number of the application.
 #
 
-# The BUILD_NUMBER env should be not empty, it comes from Jenkins.
-[ -z "{$BUILD_NUMBER}" ] && {
-    echo 'Error, BUILD_NUMBER env was empty.';
-    exit 1;
-}
-
 # Path to the php file which storing the build number
 PHP_VERSION_PATH="app/orbit_version.php"
 
@@ -19,7 +13,7 @@ PHP_VERSION_PATH="app/orbit_version.php"
 #
 # We should replace the 'XYZ' with the BUILD_NUMBER env and ABC with build date.
 echo "Bumping build number to ${BUILD_NUMBER}..."
-sed -i "s/\(ORBIT_APP_BUILD_NUMBER\x27,\)\s\(0\)/\1 $BUILD_NUMBER/" app/orbit_version.php
+sed -i "s/\(ORBIT_APP_BUILD_NUMBER\x27,\)\s\([0-9]\+\)/\1 $BUILD_NUMBER/" ${PHP_VERSION_PATH}
 
 echo "Bumping build date to ${BUILD_ID}..."
-sed -i "s/\(ORBIT_APP_BUILD_NUMBER\x27,\)\s\(0\)/\1 $BUILD_ID/" app/orbit_version.php
+sed -i "s/\(ORBIT_APP_BUILD_DATE\x27,\)\s\(\x27*\x27\));/\1 \x27$BUILD_ID\x27);/" ${PHP_VERSION_PATH}
