285ff01847a2946c1c039fc14d9eeedabee7a17e
Rio Astamal <me@rioastamal.net>
2015-03-13 10:55:03 +0800

    bump-version: Add branch creation first then pulling from github before modifying orbit_version.php

--- bump-version.sh
+++ bump-version.sh
@@ -5,7 +5,19 @@
 #
 
 # Path to the php file which storing the build number
-PHP_VERSION_PATH="app/orbit_version.php"
+export PHP_VERSION_PATH="app/orbit_version.php"
+export GIT_SSH=${JENKINS_HOME}/git-orbit-ssh.sh
+
+echo "Going int workspace directory ${WORKSPACE}..."
+cd "${WORKSPACE}"
+
+# Pulling new changes from jenkins branch
+echo "Creating branch jenkins..."
+git branch jenkins 2>/dev/null
+
+echo "Pulling new changes from jenkins branch..."
+git checkout jenkins
+ORBIT_SHOP_KEY=${ORBIT_SHOP_DEPLOY_KEY_PATH} git pull origin jenkins
 
 # We are interesting with this line
 # --> define('ORBIT_APP_BUILD_NUMBER', XYZ);
@@ -17,3 +29,10 @@ sed -i "s/\(ORBIT_APP_BUILD_NUMBER\x27,\)\s\([0-9]\+\)/\1 $BUILD_NUMBER/" ${PHP_
 
 echo "Bumping build date to ${BUILD_ID}..."
 sed -i "s/\(ORBIT_APP_BUILD_DATE\x27,\)\s\(\x27*\x27\));/\1 \x27$BUILD_ID\x27);/" ${PHP_VERSION_PATH}
+
+# Commit to the origin repository
+echo "Committing the changes for build number ${BUILD_NUMBER}..."
+git add ${PHP_VERSION_PATH} && git commit -m "Jenkins: Bump build number to ${BUILD_NUMBER}"
+
+echo "Pushing new build number ${BUILD_NUMBER} to github..."
+ORBIT_SHOP_KEY=${ORBIT_SHOP_DEPLOY_KEY_PATH} git push origin jenkins
