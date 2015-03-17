1fa5b1b292d430509fd26f1508b8572a315faba3
Ahmad Anshori <ahmad@dominopos.com>
2015-03-17 16:49:22 +0800

    Implementing #175 : Move the popup to transfer cart page

--- app/views/mobile-ci/transfer-cart.blade.php
+++ app/views/mobile-ci/transfer-cart.blade.php
@@ -47,12 +47,48 @@
       </div>
     </div>
   </div>
+
+  <div class="modal fade" id="transferFunctionModal" tabindex="-1" role="dialog" aria-labelledby="transferFunctionModalLabel" aria-hidden="true">
+    <div class="modal-dialog orbit-modal">
+      <div class="modal-content">
+        <div class="modal-header orbit-modal-header">
+          <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">{{ Lang::get('mobileci.modals.close') }}</span></button>
+          <h4 class="modal-title" id="transferFunctionModalLabel"><i class="fa fa-lightbulb-o"></i> {{ Lang::get('mobileci.modals.tip_title') }}</h4>
+        </div>
+        <div class="modal-body">
+          <p id="errorModalText">{{ Lang::get('mobileci.modals.message_transfer_cart') }}</p>
+          <img src="{{ url('mobile-ci/images/transfer_cart_tip.gif') }}" class="img-responsive">
+        </div>
+        <div class="modal-footer">
+          <div class="pull-left"><input type="checkbox" id="dismiss" name="dismiss" value="0"> {{ Lang::get('mobileci.modals.do_not_show_label') }}</div>
+          <div class="pull-right"><button type="button" class="btn btn-default" data-dismiss="modal">{{ Lang::get('mobileci.modals.close') }}</button></div>
+        </div>
+      </div>
+    </div>
+  </div>
 @stop
 
 @section('ext_script_bot')
   {{ HTML::script('mobile-ci/scripts/jquery-barcode.min.js') }}
+  {{ HTML::script('mobile-ci/scripts/jquery.cookie.js') }}
   <script type="text/javascript">
     $(document).ready(function(){
+      $('#dismiss').change(function(){
+        if($(this).is(':checked')) {
+          $.cookie('dismiss_transfercart_popup', 't', { expires: 30 });
+        } else {
+          $.cookie('dismiss_transfercart_popup', 'f', { expires: 30 });
+        }
+      });
+      if(typeof $.cookie('dismiss_transfercart_popup') === 'undefined') {
+        $.cookie('dismiss_transfercart_popup', 'f', { expires: 30 });
+        $('#transferFunctionModal').modal();
+      }
+      else{
+        if($.cookie('dismiss_transfercart_popup') == 'f') {
+          $('#transferFunctionModal').modal();
+        }
+      }
       var cart = $('#cartcode').data('cart');
       // console.log(cart);
       var setting = {
