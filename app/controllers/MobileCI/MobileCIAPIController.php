de5634f64e5b8f676b65b19ebaed91f16710a855
Ahmad Anshori <ahmad@dominopos.com>
2015-03-24 21:02:49 +0800

    MobileCI: uncomment line

--- app/controllers/MobileCI/MobileCIAPIController.php
+++ app/controllers/MobileCI/MobileCIAPIController.php
@@ -2984,8 +2984,8 @@ class MobileCIAPIController extends ControllerAPI
                             ->responseFailed()
                             ->save();
 
-            // return $this->redirectIfNotLoggedIn($e);
-                            return $e;
+            return $this->redirectIfNotLoggedIn($e);
+                            // return $e;
         }
     }
 
