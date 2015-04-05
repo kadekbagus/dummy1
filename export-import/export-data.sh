bbc807a139b9319595271145f9b12f133d142ebc
Rio Astamal <me@rioastamal.net>
2015-04-05 23:58:54 +0800

    Export-Import: Changes the way export work by automatic detection of orbit version and using orbit base dir as working directory.

--- export-import/export-data.sh
+++ export-import/export-data.sh
@@ -13,7 +13,7 @@ MYSQLUSER=
 MYSQLPASSWORD=
 MYSQLPREFIX='orb_'
 IGNORETABLES=
-ORBIT_VERSION=
+ORBIT_BASE_DIR=
 ORBIT_UPLOAD_DIR=
 
 function help() {
@@ -27,13 +27,20 @@ function help() {
     echo " -p PASSWORD           Set password of the MySQL to PASSWORD."
     echo " -e PREFIX             Set the table prefix of table name to PREFIX."
     echo " -k TABLE_1[,TABLE_N]  Skip the table named TABLE_1 and TABLE_N."
-    echo " -r VERSION            Set this export to Orbit version VERSION."
-    echo " -l UPLOAD_DIR         Location of the orbit uploads directory set to UPLOAD_DIR."
+    echo " -l BASEPATH           Location of the orbit base directory set to BASEPATH."
     echo " -h                    Print this screen and exit."
     echo ""
     echo "Example: "
-    echo "    $0 -o /var/backup -m /usr/bin/mysqldump -d orbit_shop -u abc -p 123 -r 0.11"
+    echo " $0 -o /var/backup -m /usr/bin/mysqldump \\
+ -d orbit_shop -u abc -p 123 \\
+ -l /var/www/production/orbit-shop"
     exit 1
+}
+
+function invalidarg() {
+    local MSG="${1}"
+
+    echo -e "Error: ${MSG}\n" && help
 }
 
 # Check number of arguments
@@ -41,7 +48,7 @@ if [ $# -eq 0 ]; then
     help
 fi
 
-while getopts o:m:d:u:p:r:l:e:k:h ARG;
+while getopts o:m:d:u:p:l:e:k:h ARG;
 do
     case "${ARG}" in
         o)
@@ -64,11 +71,6 @@ do
         ;;
 
         d)
-            [ -z "${OPTARG}" ] && {
-                echo "Database name is empty."
-                exit 2
-            }
-
             MYSQLDB=${OPTARG}
         ;;
 
@@ -103,24 +105,18 @@ do
             }
         ;;
 
-        r)
-           # Orbit version
-           [ -z "${OPTARG}" ] && {
-               echo "You need to specify orbit version."
-               exit 3
-           }
-
-           ORBIT_VERSION=${OPTARG}
-        ;;
-
         l)
             # Orbit uploads dir
-            [ -d "${OPTARG}" ] || {
-                echo "Could not find orbit uploads/ directory at ${OPTARG}."
+            ORBIT_BASE_DIR=$( readlink -f ${OPTARG} )
+
+            # Check for file app/orbit_version.php to determine whether we are
+            # on the right directory.
+            [ -f "${ORBIT_BASE_DIR}/app/orbit_version.php" ] || {
+                echo "It seems your orbit base directory is invalid."
                 exit 3
             }
 
-            ORBIT_UPLOAD_DIR="${OPTARG}"
+            ORBIT_UPLOAD_DIR="${ORBIT_BASE_DIR}/public"
         ;;
 
         *)
@@ -128,6 +124,26 @@ do
         ;;
     esac
 done
+
+# Check each of arguments before starting to do the import process.
+
+# Check source file
+[ -z "${TARGET_DIR}" ] && invalidarg "You need to specify source output directory using -o."
+
+# Check mysqldump binary path
+[ -z "${MYSQLDUMP_PATH}" ] && invalidarg "You need to specify mysqldump binary path using -m."
+
+# Check MySQL database name
+[ -z "${MYSQLDB}" ] && invalidarg "You need to specify mysql database name using -d."
+
+# Check MySQL user
+[ -z "${MYSQLUSER}" ] && invalidarg "You need to specify mysql user using -u."
+
+# Check MySQL password
+[ -z "${MYSQLPASSWORD}" ] && invalidarg "You need to specify mysql password using -p."
+
+# Check Orbit base directory
+[ -z "${ORBIT_BASE_DIR}" ] && invalidarg "You need to specify orbit base directory using -l."
 
 # Function to to dump orbit mysql data
 function orbitdump_sql()
@@ -140,29 +156,40 @@ function orbitdump_sql()
     local MPREFIX=$6
 
     # Run mysqldump to export the database structure
-    echo "Dumping the structure of orbit database..."
-    ${MYSQLDUMP} -u ${MUSER} -p${MPASS} --verbose ${MDB} --no-data > ${TARGET}-struct.sql || {
+    echo -n "Dumping the structure of orbit database..."
+    ${MYSQLDUMP} -u ${MUSER} -p${MPASS} ${MDB} --no-data > ${TARGET}-struct.sql || {
         echo "Failed dumping the database structure.";
         exit 4;
     }
-    echo "Dumping database structure done."
+    echo "done."
 
     # Run mysqldump to export the data excluding user activities
-    echo "Dumping orbit database data..."
-    ${MYSQLDUMP} -u ${MUSER} -p${MPASS} --verbose --no-create-info ${IGNORETABLES} ${MDB} > ${TARGET}-data.sql || {
+    echo -n "Dumping orbit database data..."
+    ${MYSQLDUMP} -u ${MUSER} -p${MPASS} --no-create-info ${IGNORETABLES} ${MDB} > ${TARGET}-data.sql || {
         echo "Failed dumping the database data.";
         exit 4;
 
     }
-    echo "Dumping database data done."
+    echo "done."
 }
 
-# Create a directory named $TARGET_DIR/processing.$MYPID
-# So we know that the export proces is not done yet.
+# Get the version of current orbit
+# The #) at the end are just to make syntax highlighting behaves correctly
+ORBIT_VERSION=$( grep "define('ORBIT_APP_VERSION" ${ORBIT_BASE_DIR}/app/orbit_version.php | sed "s/define('ORBIT_APP_VERSION', '\(.*\)');/\1/" ) #)
+
+# Trim the whitespace
+ORBIT_VERSION=$( echo ${ORBIT_VERSION} )
+
+# Full path to the target directory
 TARGET_DIR_FULLPATH=$( readlink -f ${TARGET_DIR} )
+
+# Archive name
 ARCHIVE_NAME=orbit-dump-${ORBIT_VERSION}.$( date +%Y%m%dT%H%M%S )
-PROCESSING_DIR_NAME=${TARGET_DIR_FULLPATH}/processing/${ARCHIVE_NAME}.${MYPID}
-DONE_DIR_NAME="${TARGET_DIR_FULLPATH}/done/"
+
+# Create a directory named $TARGET_DIR/export/processing.$MYPID
+# So we know that the export proces is not done yet.
+PROCESSING_DIR_NAME=${TARGET_DIR_FULLPATH}/export/processing/${ARCHIVE_NAME}.${MYPID}
+DONE_DIR_NAME="${TARGET_DIR_FULLPATH}/export/done/"
 
 mkdir -p ${PROCESSING_DIR_NAME} 2>/dev/null || {
     echo "Failed creating 'processing' dirname: ${PROCESSING_DIR_NAME}";
@@ -176,20 +203,27 @@ orbitdump_sql ${PROCESSING_DIR_NAME}/orbit-${ORBIT_VERSION} \
               "${MYSQLDUMP_PATH}" "${MYSQLDB}" "${MYSQLUSER}" \
               "${MYSQLPASSWORD}" "${MYSQLPREFIX}"
 
-cp -R ${ORBIT_UPLOAD_DIR}/ ${PROCESSING_DIR_NAME}/
+echo -n "Copying uploads directory..."
+cp -R ${ORBIT_UPLOAD_DIR}/uploads ${PROCESSING_DIR_NAME}/ || {
+    echo "Failed."
+    exit 6;
+}
+echo "done."
 
 echo ${ORBIT_VERSION} > ${PROCESSING_DIR_NAME}/orbit-version.txt
 
 # Create tar archive
+echo -n "Creating archive for export..."
 cd ${PROCESSING_DIR_NAME} && \
-tar -zcvf ${ARCHIVE_NAME}.tar.gz \
+tar -zcf ${ARCHIVE_NAME}.tar.gz \
     orbit-version.txt orbit-${ORBIT_VERSION}-struct.sql \
     orbit-${ORBIT_VERSION}-data.sql uploads
+echo "done."
 
 # If we goes here then the process is done. We need to move the archive to the
 # done directory.
 mv ${PROCESSING_DIR_NAME}/${ARCHIVE_NAME}.tar.gz ${DONE_DIR_NAME}/${ARCHIVE_NAME}.tar.gz
-rm -rf ${PROCESSING_DIR_NAME}
+# rm -rf ${PROCESSING_DIR_NAME}
 
 echo "Export is done."
 exit 0
