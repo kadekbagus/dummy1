9f85d3390e310c994420809f83acedcb4f6ea12d
Tian <tian@dominopos.com>
2015-04-10 11:43:56 +0800

    Fix Activity date filter.

--- app/controllers/api/v1/ActivityAPIController.php
+++ app/controllers/api/v1/ActivityAPIController.php
@@ -83,19 +83,19 @@ class ActivityAPIController extends ControllerAPI
             $start_date = OrbitInput::get('start_date');
             $end_date = OrbitInput::get('end_date');
 
-            $tomorrow = date('Y-m-d', strtotime('tomorrow'));
+            $tomorrow = date('Y-m-d H:i:s', strtotime('tomorrow'));
             $validator = Validator::make(
                 array(
                     'sort_by'       => $sort_by,
                     'merchant_ids'  => OrbitInput::get('merchant_ids'),
-                    'start_date'     => $start_date,
+                    'start_date'    => $start_date,
                     'end_date'      => $end_date
                 ),
                 array(
                     'sort_by'       => 'in:id,ip_address,created,registered_at,email,full_name,object_name,product_name,coupon_name,promotion_name,event_name,action_name,action_name_long,activity_type,gender,staff_name,module_name',
                     'merchant_ids'  => 'orbit.check.merchants',
-                    'start_date'    => 'date_format:Y-m-d|before:' . $tomorrow,
-                    'end_date'      => 'date_format:Y-m-d|before:' . $tomorrow,
+                    'start_date'    => 'date_format:Y-m-d H:i:s|before:' . $tomorrow,
+                    'end_date'      => 'date_format:Y-m-d H:i:s|before:' . $tomorrow,
                 ),
                 array(
                     'in' => Lang::get('validation.orbit.empty.activity_sortby'),
