57bb1ac8368eb12c58e9547c0956ef1a620be5c8
Tian <tian@dominopos.com>
2015-05-18 16:03:27 +0800

    Fix filter expiration_end_date for coupon search

--- app/controllers/api/v1/CouponAPIController.php
+++ app/controllers/api/v1/CouponAPIController.php
@@ -1516,8 +1516,15 @@ class CouponAPIController extends ControllerAPI
             OrbitInput::get('expiration_end_date', function($enddate) use ($coupons)
             {
                 $coupons->where(function ($q) use ($enddate) {
-                    $q->where('promotions.end_date', '<=', $enddate)
-                      ->orWhere('promotions.is_permanent', 'Y');
+                    $q->where(function($q) use ($enddate) {
+                        $q->where('promotions.end_date', '<=', $enddate)
+                          ->where('promotions.begin_date', '<=', $enddate)
+                          ->where('promotions.is_permanent', 'N');
+                      })
+                      ->orWhere(function($q) use ($enddate) {
+                            $q->where('promotions.begin_date', '<=', $enddate)
+                              ->where('promotions.is_permanent', 'Y');
+                      });
                 });
             });
 
