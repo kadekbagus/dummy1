f4ecf236ff7b07afb7b7115f1b74c1c4b67888a0
Yudi Rahono <yudi@rahono.com>
2015-05-26 17:53:20 +0800

    FIX: OP-123, OP-128, OP-130, OP-131

--- app/views/printer/list-cashier-time-view.php
+++ app/views/printer/list-cashier-time-view.php
@@ -99,7 +99,6 @@
             <option value="20">24</option>
         </select>
         <button id="printbtn" style="padding:0 6px;" onclick="window.print()">Print Page</button>
-        <button id="printbtn" style="padding:0 6px;" onclick="window.exportToCSV()">Export to CSV</button>
     </div>
     <div id="loadingbar">Loading all the data, please wait...</div>
 </div>
@@ -126,18 +125,18 @@
 
     <table style="width:100%">
         <thead>
-        <th style="text-align: left;">'No.'</th>
-        <th style="text-align: left;">'Date'</th>
-        <th style="text-align: left;">'Employee Name'</th>
-        <th style="text-align: left;">'Clock In'</th>
-        <th style="text-align: left;">'Clock Out'</th>
-        <th style="text-align: left;">'Total Time'</th>
-        <th style="text-align: left;">'Number Of Receipt'</th>
-        <th style="text-align: left;">'Total Sales'</th>
+        <th style="text-align: left;">No.</th>
+        <th style="text-align: left;">Date</th>
+        <th style="text-align: left;">Employee Name</th>
+        <th style="text-align: left;">Clock In</th>
+        <th style="text-align: left;">Clock Out</th>
+        <th style="text-align: left;">Total Time</th>
+        <th style="text-align: left;">Number Of Receipt</th>
+        <th style="text-align: left;">Total Sales</th>
         </thead>
         <tbody>
         <?php while ($row = $statement->fetch(PDO::FETCH_OBJ)) : ?>
-            <tr class="{{ $rowCounter % 2 === 0 ? 'zebra' : '' }}">
+            <tr class="<?php echo $rowCounter % 2 === 0 ? 'zebra' : '' ?>">
                 <td><?php echo (++$rowCounter); ?></td>
                 <td><?php echo ($row->activity_date); ?></td>
                 <td><?php echo ($row->activity_full_name); ?></td>
@@ -145,7 +144,7 @@
                 <td><?php echo ($formatDate($row->logout_at)); ?></td>
                 <td><?php echo ($totalTime($row->login_at, $row->logout_at)); ?></td>
                 <td><?php echo ($row->transactions_count); ?></td>
-                <td><?php echo ($row->transactions_total); ?></td>
+                <td><?php echo number_format($row->transactions_total, 2); ?></td>
             </tr>
         <?php endwhile; ?>
         </tbody>
@@ -168,13 +167,6 @@
         document.getElementById('main').style.fontFamily = "Arial";
         document.getElementById('main').style.fontSize = "12px";
         document.getElementById('loadingbar').style.display = 'none';
-    }
-
-    function exportToCSV() {
-        // Replace the redundant query string argument 'export'
-        var url = window.location.href.replace('&export=print', '').replace('&export=csv', '');
-
-        window.location.href = url + '&export=csv';
     }
 </script>
 
