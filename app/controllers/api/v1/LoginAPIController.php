58d20373bfe473182dca7dca7d48aef382146e35
Tian <tian@dominopos.com>
2015-06-04 15:06:59 +0800

    Fix merchant login to check user role=Merchant Owner

--- app/controllers/api/v1/LoginAPIController.php
+++ app/controllers/api/v1/LoginAPIController.php
@@ -242,6 +242,9 @@ class LoginAPIController extends ControllerAPI
 
             $user = User::with('userdetail', 'role', 'merchants')
                         ->active()
+                        ->whereHas('role', function($q) {
+                            $q->where('role_name', 'Merchant Owner');
+                        })
                         ->where('user_email', $email)
                         ->first();
 
