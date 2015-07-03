cd8b907b617333d9ea24b82f2ec09ef693a2a002
Ahmad Anshori <ahmad@dominopos.com>
2015-07-03 12:13:24 +0800

    Disable login btn when signing in

--- app/lang/id/mobileci.php
+++ app/lang/id/mobileci.php
@@ -86,6 +86,7 @@ return array(
         'not'               => 'Bukan',
         'click_here'        => 'klik disini',
         'email_not_valid'   => 'Alamat email tidak valid',
+        'logging_in_button' => 'Sedang login. Harap tunggu...',
     ),
 
     'catalogue'         => array(
