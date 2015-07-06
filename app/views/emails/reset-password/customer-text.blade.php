3660fafb8ee8b627ece50eb639011ba5316a8a56
William Shallum <william@dominopos.com>
2015-07-06 16:36:29 +0800

    Password Reset (OP-188)

---
+++ app/views/emails/reset-password/customer-text.blade.php
@@ -0,0 +1,19 @@
+Hi {{ $email }},
+
+Someone (hopefully you) recently requested a password reset.
+
+To reset your password, follow the link below:
+
+{{ $token_url }}
+
+
+Regards,
+Orbit Team
+
+-----------------------------------------------------------
+ORBIT CUSTOMER SERVICE
+
+Phone: {{ $cs_phone }} ({{ $cs_office_hour }})
+Email: {{ $cs_email }}
+------------------------------------------------------------
+
