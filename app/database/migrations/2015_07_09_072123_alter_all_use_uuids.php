66cb0e1ae3879db5d7186a0f0a0a7cd5d686cd0d
William Shallum <william@dominopos.com>
2015-07-09 15:44:25 +0800

    Some sort of ALTER

---
+++ app/database/migrations/2015_07_09_072123_alter_all_use_uuids.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterAllUseUuids extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        $builder = DB::connection()->getSchemaBuilder();
+        $builder->blueprintResolver(function ($table, $callback) {
+            return new OrbitBlueprint($table, $callback);
+        });
+        $all_id_columns = DB::select(
+                'SELECT table_name, column_name, is_nullable ' .
+                'FROM information_schema.columns '.
+                'WHERE table_schema = ? AND (column_name LIKE ? OR column_name = ?)', [DB::connection()->getDatabaseName(), '%_id', 'modified_by']);
+        foreach ($all_id_columns as $col) {
+            $stmt = '';
+            if ($col->is_nullable == 'YES') {
+                $stmt = ("ALTER TABLE `{$col->table_name}` MODIFY `{$col->column_name}` BINARY(16);");
+            }
+            else {
+                $stmt = ("ALTER TABLE `{$col->table_name}` MODIFY `{$col->column_name}` BINARY(16) NOT NULL;");
+            }
+            $ok = DB::statement($stmt);
+            if (!$ok) {
+                throw \Exception("FAIL: " . $stmt);
+            }
+        }
+	}
+
+}
