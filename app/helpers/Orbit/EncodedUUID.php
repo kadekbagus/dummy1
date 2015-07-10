176f01cbbfb04ea2579ca19ff6b365aad294fa5e
William Shallum <william@dominopos.com>
2015-07-10 11:49:33 +0800

    use char(22) for encoding uuids

---
+++ app/helpers/Orbit/EncodedUUID.php
@@ -0,0 +1,23 @@
+<?php
+namespace Orbit;
+
+
+class EncodedUUID {
+
+    const CHARS_D64 = ".0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz-";
+    const CHARS_B64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+    public static function make() {
+        $u = uuid_create(UUID_TYPE_TIME);
+        $hex = substr($u, 14, 4) . substr($u, 9, 4) . substr($u, 0, 8) . substr($u, 19, 4) . substr($u, 24);
+        return rtrim(strtr(base64_encode(hex2bin($hex)), EncodedUUID::CHARS_B64, EncodedUUID::CHARS_D64), '-');
+    }
+
+    public static function makeMany($count) {
+        $result = [];
+        for ($i = 0; $i < $count; $i++) {
+            $result[] = static::make();
+        }
+        return $result;
+    }
+}
