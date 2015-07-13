0f4516111f5693f761db62d85abfe63e29213884
William Shallum <william@dominopos.com>
2015-07-13 16:38:42 +0800

    change customer portal reset & resend password URL to /app from /api.

--- app/routes/05-customerportal/01-login.php
+++ app/routes/05-customerportal/01-login.php
@@ -20,10 +20,10 @@ Route::post('/app/v1/pos/logout', 'IntermediateLoginController@Customerportal\Cu
 
 
 // customer resend activation
-Route::post('/api/v1/customerportal/resend-activation-email', 'Customerportal\CustomerportalAPIController@postResendActivationEmail');
+Route::post('/app/v1/customerportal/resend-activation-email', 'Customerportal\CustomerportalAPIController@postResendActivationEmail');
 
 // customer reset password
-Route::post('/api/v1/customerportal/request-password-reset', 'Customerportal\CustomerportalAPIController@postRequestPasswordReset');
-Route::post('/api/v1/customerportal/reset-password', 'Customerportal\CustomerportalAPIController@postResetPassword');
+Route::post('/app/v1/customerportal/request-password-reset', 'Customerportal\CustomerportalAPIController@postRequestPasswordReset');
+Route::post('/app/v1/customerportal/reset-password', 'Customerportal\CustomerportalAPIController@postResetPassword');
 
 
