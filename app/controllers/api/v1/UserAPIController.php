c1e833b2988cfac394b1919250139ea387545bf8
kadekbagus <kadek@dominopos.com>
2015-07-14 17:56:02 +0800

    fix query slow on consumer listing

--- app/controllers/api/v1/UserAPIController.php
+++ app/controllers/api/v1/UserAPIController.php
@@ -1248,23 +1248,21 @@ class UserAPIController extends ControllerAPI
                     ->select('users.*',
                              DB::raw("GROUP_CONCAT(`{$prefix}personal_interests`.`personal_interest_value` SEPARATOR ', ') as personal_interest_list"),
                              DB::raw("(SELECT m.name 
-                                        FROM {$prefix}activities at 
-                                        INNER JOIN {$prefix}merchants m on m.merchant_id=at.location_id 
-                                        WHERE 
-                                            at.user_id={$prefix}users.user_id AND 
-                                            at.activity_name='login_ok' AND
-                                            at.group = 'mobile-ci' 
-                                        GROUP BY at.created_at 
-                                        ORDER BY at.created_at DESC LIMIT 1) as last_visited_store"),
-                            DB::raw("(SELECT at.created_at 
-                                        FROM {$prefix}activities at 
-                                        INNER JOIN {$prefix}merchants m on m.merchant_id=at.location_id 
-                                        WHERE 
-                                            at.user_id={$prefix}users.user_id AND 
-                                            at.activity_name='login_ok' AND
-                                            at.group = 'mobile-ci' 
-                                        GROUP BY at.created_at 
-                                        ORDER BY at.created_at DESC LIMIT 1) as last_visited_date"),
+                                    FROM {$prefix}activities at 
+                                    LEFT JOIN {$prefix}merchants m on m.merchant_id=at.location_id 
+                                    WHERE 
+                                        at.user_id={$prefix}users.user_id AND 
+                                        at.activity_name='login_ok' AND
+                                        at.group = 'mobile-ci'
+                                    ORDER BY at.created_at DESC LIMIT 1) as last_visited_store"),
+                             DB::raw("(SELECT at.created_at 
+                                    FROM {$prefix}activities at 
+                                    LEFT JOIN {$prefix}merchants m2 on m2.merchant_id=at.location_id 
+                                    WHERE 
+                                        at.user_id={$prefix}users.user_id AND 
+                                        at.activity_name='login_ok' AND
+                                        at.group = 'mobile-ci'
+                                    ORDER BY at.created_at DESC LIMIT 1) as last_visited_date"),
                              DB::raw("(SELECT tr.total_to_pay FROM {$prefix}transactions tr WHERE customer_id={$prefix}users.user_id and status='paid' GROUP BY tr.created_at ORDER BY tr.created_at DESC LIMIT 1) as last_spent_amount")
                             )
                     ->join('user_details', 'user_details.user_id', '=', 'users.user_id')
