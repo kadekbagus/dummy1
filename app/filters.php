4ecbddb7baf86edb786ea1d2e2d5e54122c211be
Yudi Rahono <yudi@rahono.com>
2015-08-04 15:34:04 +0800

    FIX: remove debuging info

--- app/filters.php
+++ app/filters.php
@@ -141,26 +141,3 @@ Route::filter('enable-cart', function()
         return Redirect::route('home');
     }
 });
-
-Event::listen('illuminate.query', function($query, $bindings, $time, $name) {
-    $data = compact('bindings', 'time', 'name');
-
-    // Format binding data for sql insertion
-    foreach ($bindings as $i => $binding) {
-        if ($binding instanceof \DateTime) {
-            $bindings[$i] = $binding->format('\'Y-m-d H:i:s\'');
-        } else if (is_string($binding)) {
-            $bindings[$i] = "'$binding'";
-        }
-    }
-
-    // Insert bindings into query
-    $query = str_replace(array('%', '?'), array('%%', '%s'), $query);
-    $query = vsprintf($query, $bindings);
-
-    $log = new Logger('sql');
-    $log->pushHandler(new StreamHandler(storage_path().'/logs/query.log', Logger::INFO));
-
-    // add records to the log
-    $log->addInfo($query, $data);
-});
