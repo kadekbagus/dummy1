8f03267c5d30fc9bd4ec4d97f5e70c5a1d075774
William Shallum <william@dominopos.com>
2015-08-10 10:38:03 +0800

    OrbitBlueprint: Remove binary, UUID: not used.

--- app/models/OrbitBlueprint.php
+++ app/models/OrbitBlueprint.php
@@ -5,19 +5,8 @@
  */
 class OrbitBlueprint extends \Illuminate\Database\Schema\Blueprint
 {
-    public function binaryString($column, $length = 128)
+    public function encodedId($column)
     {
-        return $this->addColumn('binaryString', $column, compact('length'));
-    }
-
-    public function binaryUuid($column)
-    {
-        return $this->binaryString($column, 16); // 128 bits = 16 bytes
-    }
-
-    public function encodedUuid($column)
-    {
-        // 3TJa fUYD bTuF 85Yr 93Ip LV
-        return $this->addColumn('char', $column, ['length' => 22, 'character_set' => 'ascii', 'collation' => 'ascii_bin']);
+        return $this->addColumn('char', $column, ['length' => 16, 'character_set' => 'ascii', 'collation' => 'ascii_bin']);
     }
 }
