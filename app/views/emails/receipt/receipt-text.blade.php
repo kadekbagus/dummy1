9289e84b2fee116ec4bec52259d5e9b0fc0d97e4
Ahmad Anshori <ahmad@dominopos.com>
2015-09-07 16:48:06 +0800

    Fix unbound variable

--- app/views/emails/receipt/receipt-text.blade.php
+++ app/views/emails/receipt/receipt-text.blade.php
@@ -5,7 +5,7 @@ Here is a copy of your ticket and some information about your products.
 Please come see us again soon!
 
 @foreach($transactiondetails as $detail)
-● {{ $detail->product->post_sales_url }} : {{ $detail->product_name }} @if(! is_null($detail->product_attribute_value1)) / {{ $detail->product_attribute_value1 }} @endif @if(! is_null($detail->product_attribute_value2)) / {{ $detail->product_attribute_value2 }} @endif @if(! is_null($detail->product_attribute_value3)) / {{ $detail->product_attribute_value3 }} @endif @if(! is_null($detail->product_attribute_value4)) / {{ $detail->product_attribute_value4 }} @endif @if(! is_null($detail->product_attribute_value5)) / {{ $detail->product_attribute_value5 }} @endif @if(! empty($detail->variant_sku)) / $detail->variant_sku @endif
+● {{ $detail->product->post_sales_url }} : {{ $detail->product_name }} @if(! is_null($detail->product_attribute_value1)) / {{ $detail->product_attribute_value1 }} @endif @if(! is_null($detail->product_attribute_value2)) / {{ $detail->product_attribute_value2 }} @endif @if(! is_null($detail->product_attribute_value3)) / {{ $detail->product_attribute_value3 }} @endif @if(! is_null($detail->product_attribute_value4)) / {{ $detail->product_attribute_value4 }} @endif @if(! is_null($detail->product_attribute_value5)) / {{ $detail->product_attribute_value5 }} @endif @if(! empty($detail->variant_sku)) / {{ $detail->variant_sku }} @endif
 
 
 @endforeach
