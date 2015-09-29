8d904eb148b04fe4e0e7f1108c25edf335524b95
Yudi Rahono <yudi@rahono.com>
2015-09-29 15:50:31 +0800

    FIX: copy some new migration for langaes from mall

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -14,14 +14,14 @@ class DatabaseSeeder extends Seeder
 
         DB::connection()->getPdo()->beginTransaction();
 
-        $this->call('RoleTableSeeder');
-        $this->call('PermissionTableSeeder');
-        $this->call('PermissionRoleTableSeeder');
-        $this->call('UserTableSeeder');
+        /* $this->call('RoleTableSeeder'); */
+        /* $this->call('PermissionTableSeeder'); */
+        /* $this->call('PermissionRoleTableSeeder'); */
+        /* $this->call('UserTableSeeder'); */
         $this->call('MerchantDataSeeder');
-        $this->call('CountryTableSeeder');
-        $this->call('PersonalInterestTableSeeder');
-        $this->call('SettingTableSeeder');
+        /* $this->call('CountryTableSeeder'); */
+        /* $this->call('PersonalInterestTableSeeder'); */
+        /* $this->call('SettingTableSeeder'); */
 
         DB::connection()->getPdo()->commit();
 
