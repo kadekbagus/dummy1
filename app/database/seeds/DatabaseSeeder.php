c679e7174ec44df54d21c53ab927d41b48236221
Yudi Rahono <yudi@rahono.com>
2015-10-02 17:14:07 +0800

    FIX: commented seeders

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -9,19 +9,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        try {
+            $this->doRun();
+        } catch(Exception $e)
+        {
+            $this->command->error($e);
+        }
+    }
+
+    public function doRun()
+    {
         Eloquent::unguard();
 
 
         DB::connection()->getPdo()->beginTransaction();
 
-        /* $this->call('RoleTableSeeder'); */
-        /* $this->call('PermissionTableSeeder'); */
-        /* $this->call('PermissionRoleTableSeeder'); */
-        /* $this->call('UserTableSeeder'); */
+        $this->call('RoleTableSeeder');
+        $this->call('PermissionTableSeeder');
+        $this->call('PermissionRoleTableSeeder');
+        $this->call('UserTableSeeder');
         $this->call('MerchantDataSeeder');
-        /* $this->call('CountryTableSeeder'); */
-        /* $this->call('PersonalInterestTableSeeder'); */
-        /* $this->call('SettingTableSeeder'); */
+        $this->call('CountryTableSeeder');
+        $this->call('PersonalInterestTableSeeder');
+        $this->call('SettingTableSeeder');
 
         DB::connection()->getPdo()->commit();
 
